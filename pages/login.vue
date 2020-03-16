0d6cc3aeb712d13b90c8c0a4f87b4b3b9477a3a5
rtr-x8 <ryu.2.24@hotmail.co.jp>
2020-03-16 14:46:12 +0900

    aaa

---
+++ pages/login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p v-for="(provider, i) in providers" :key="i">
+      <v-btn
+        large
+        block
+        :href="redirectUrl(provider.name)"
+        style="text-transform:capitalize"
+        :color="provider.color"
+        :disabled="provider.disable"
+      >
+        {{ provider.name }}でログイン
+      </v-btn>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      providers: [
+        {
+          name: 'twitter',
+          color: '#1DA1F2',
+          disable: false
+        },
+        {
+          name: 'github',
+          color: '#24292E',
+          disable: true
+        }
+      ]
+    }
+  },
+  methods: {
+    redirectUrl (provider) {
+      return '/oauth/redirect/' + provider
+    }
+  }
+}
+</script>
